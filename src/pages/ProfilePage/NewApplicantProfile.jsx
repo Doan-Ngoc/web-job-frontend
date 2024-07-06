ad874d89c792dcc0d630ce94e6295115480af550
Doan-Ngoc <minhngocdoan3112@gmail.com>
2024-07-06 16:03:41 +0700

    Design upload photo input

--- src/pages/ProfilePage/NewApplicantProfile.jsx
+++ src/pages/ProfilePage/NewApplicantProfile.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, Textarea } from '@material-tailwind/react';
+import { Button, Input, Textarea, Card, Typography } from '@material-tailwind/react';
 import { useForm } from 'react-hook-form';
 import { useState, useEffect, useContext } from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -54,81 +54,102 @@ export default function NewApplicantProfile() {
         }
       });
       const res = await applicantApi.createApplicantProfile(formData)
-        toast.success('Your profile is created!');
+      toast.success('Your profile is created!');
     }
     catch (err) {
       console.error("Signing up failed", err.message);
       const errorMessage = err.response?.data?.message || ' Oops something went wrong! ';
       toast.error(errorMessage);
     }
-    
+
 
   }
 
   return (
-    <FormWrapper
+    <div className="w-screen flex items-center justify-center">
+      <Card color="transparent" className="items-center p-12 shadow-2xl">
+        <form
+          className="mt-8 mb-2 sm:w-96 flex-col justify-evenly gap-2"
+          onSubmit={handleSubmit(onSubmit)} >
+          {/* <FormWrapper
       title="New Applicant Profile"
       description="Tell us more about yourself"
       // encType="multipart/form-data"
       onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="flex flex-col gap-6 ">
-        <InputWrapper error={errors.profilePicture}>
+    > */}
+          <div className="mb-8">
+            <Typography variant="h4" color="blue-gray" className="text-center">
+              New Applicant Profile
+            </Typography>
+            <Typography color="gray" className="mt-1 font-normal text-center">
+              Tell us more about yourself
+            </Typography>
+          </div>
+
+          <div className='flex flex-col items-center justify-center mb-1'>
+          <img src="http://localhost:3000/uploads/profilePictures/applicantAvatars/default-avatar.jpg" 
+          className="w-44 h-44 rounded-full my-6" />
+          </div>
+          {/* <InputWrapper error={errors.profilePicture}>
           <Input size="lg" type="file" label="Profile Picture" {...register('profilePicture')} />
-        </InputWrapper>
-        <InputWrapper error={errors.name}>
-          <Input size="lg" type="text" label="Full Name" {...register('name')} />
-        </InputWrapper>
-        <InputWrapper error={errors.phone}>
-          <Input
-            size="lg"
-            type="tel"
-            label="Phone number"
-            {...register('phone')}
-          />
-        </InputWrapper>
-        <InputWrapper error={errors.email}>
-          <Input size="lg" type="text" label="Email" {...register('email')} />
-        </InputWrapper>
-        <InputWrapper error={errors.workingFields}>
-          <label className="pb-2">
-            Working Fields
-          </label>
-          <div className="dropdown w-full">
-            <div tabIndex={0} role="button" className="btn m-1 w-full">
-              Select options
-              <ion-icon name="caret-down"></ion-icon>
-            </div>
-            <div tabIndex={0} className="form-control dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-b-lg w-full">
-              <div className="max-h-60 overflow-y-auto">
-                {jobFields.map((field) => (
-                  <label className="label cursor-pointer" key={field}>
-                    <span className="label-text">{field}</span>
-                    <input type="checkbox" value={field}
-                      className="checkbox checkbox-primary"
-                      {...register("workingFields")} />
-                  </label>
-                ))}
+        <p className="pl-2">JPG or PNG. (Max 3MB)</p>
+        </InputWrapper>  */}
+          <InputWrapper error={errors.name}>
+            <Input size="lg" type="text" label="Full Name" {...register('name')} />
+          </InputWrapper>
+          <InputWrapper error={errors.phone}>
+            <Input
+              size="lg"
+              type="tel"
+              label="Phone number"
+              {...register('phone')}
+            />
+          </InputWrapper>
+          <InputWrapper error={errors.email}>
+            <Input size="lg" type="text" label="Email" {...register('email')} />
+          </InputWrapper>
+          <InputWrapper error={errors.workingFields}>
+            <label className="pb-2">
+              Working Fields
+            </label>
+            <div className="dropdown w-full">
+              <div tabIndex={0} role="button" className="btn m-1 w-full">
+                Select options
+                <ion-icon name="caret-down"></ion-icon>
+              </div>
+              <div tabIndex={0} className="form-control dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-b-lg w-full">
+                <div className="max-h-60 overflow-y-auto">
+                  {jobFields.map((field) => (
+                    <label className="label cursor-pointer" key={field}>
+                      <span className="label-text">{field}</span>
+                      <input type="checkbox" value={field}
+                        className="checkbox checkbox-primary"
+                        {...register("workingFields")} />
+                    </label>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        </InputWrapper>
-        <InputWrapper error={errors.description}>
-          <Textarea
-            size="lg"
-            type="text"
-            label="Self Introduction"
-            {...register('description')}
-          />
-        </InputWrapper>
-        <InputWrapper error={errors.applicantCV}>
-        <label className="pb-2">Upload your CV:</label>
-          <Input size="lg" type="file" {...register('applicantCV')} />
-        </InputWrapper>
-      </div>
-      <Button type="submit" className="mt-6" fullWidth>
-        Create Profile
-      </Button>
-    </FormWrapper>
+          </InputWrapper>
+          <InputWrapper error={errors.description}>
+            <Textarea
+              size="lg"
+              type="text"
+              label="Self Introduction"
+              {...register('description')}
+            />
+          </InputWrapper>
+          <InputWrapper error={errors.applicantCV}>
+            <label className="pb-2">Upload your CV (PDF only):</label>
+            <Input size="lg" type="file" {...register('applicantCV')} />
+          </InputWrapper>
+
+          <Button type="submit" className="mt-6" fullWidth>
+            Create Profile
+          </Button>
+          {/* </FormWrapper> */}
+        </form>
+      </Card>
+    </div>
   )
 }
