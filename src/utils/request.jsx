79526203a791bc9a71e4577418e9bcf0cd9ed62c
Minh Ngoc Doan <minhngocdoan3112@gmail.com>
2025-03-04 00:08:35 +0700

    Move base url to env file

--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -1,12 +1,8 @@
 import axios from 'axios';
-import { jwtDecode } from "jwt-decode";
-import { useAuth } from '../hooks/useAuth';
 import * as authApi from '../api/authenticate'
-import { useState } from 'react';
 
 export const request = axios.create({
-  baseURL: 'http://localhost:3000',
-  // baseURL: "https://job-connect-api.onrender.com",
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
@@ -14,36 +10,6 @@ export const request = axios.create({
   timeout: 100000,
   withCredentials: true
 });
-
-// Attach access token to requests
-// request.interceptors.request.use((config) => {
-//   const {accessToken} = useAuth();
-//   if (accessToken) {
-//       config.headers.Authorization = `Bearer ${accessToken}`;
-//   }
-//   return config;
-// });
-//----------
-// Store setAccessToken globally
-
-// export const setAuthToken = (token) => {
-//   if (token) {
-//     request.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-//   } else {
-//     delete request.defaults.headers.common["Authorization"];
-//   }
-// };
-
-// let [authToken, setAuthToken] = useState()
-
-// request.interceptors.request.use((config) => {
-//   if (accessToken) {
-//     config.headers["Authorization"] = `Bearer ${accessToken}`;
-//   } else {
-//     delete config.headers["Authorization"];
-//   }
-//   return config;
-// });
 
 // Allow setting setAccessToken from context
 let updateAccessToken = null;
@@ -61,7 +27,6 @@ request.interceptors.response.use(
         const newAccessToken = await authApi.refreshAccessToken();
         if (newAccessToken) {
         updateAccessToken(newAccessToken); // ✅ Update token in context
-        // setAuthToken(newAccessToken);
         // Retry original request with new token
         error.config.headers["Authorization"] = `Bearer ${newAccessToken}`;
         return axios(error.config);
@@ -75,48 +40,3 @@ request.interceptors.response.use(
   }
 );
 
-//Reissue access token when expired
-// request.interceptors.response.use(
-//   (response) => response, // Return response if no error
-//   async (error) => {
-//       if (error.response?.status === 401) {
-//           try {
-//               const {setAccessToken} = useAuth();
-//               const newAccessToken = await authApi.refreshAccessToken(setAccessToken);
-//               error.config.headers.Authorization = `Bearer ${newAccessToken}`;
-//               return axios(error.config); // Retry request
-//           } catch (err) {
-//               console.error("Session expired. Please log in again.");
-//               return Promise.reject(err);
-//           }
-//       }
-//       return Promise.reject(error);
-//   }
-// );
-
-// request.interceptors.request.use(async (config) => {
-//   if (config.url === '/auth/logout' || config.url === '/auth/token/refresh') {
-//     return config;
-//   }
-//   let accessToken = localStorage.getItem('accessToken');
-// //   // Check if access token is expired and refresh it if needed
-// if (accessToken) {
-//   if (isTokenExpired(accessToken)) {
-//     const response = await authApi.refreshAccessToken()
-//   }
-// }
-//   return config;
-// });
-
-// // Function to check if the token is expired
-// function isTokenExpired(token) {
-//   try {
-//     const decodedToken = jwtDecode(token);
-//     const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
-//     // Check if the token has expired
-//     return decodedToken.exp < currentTime;
-//   } catch (error) {
-//     console.error("Failed to decode token:", error);
-//     return true;
-//   }
-// }
